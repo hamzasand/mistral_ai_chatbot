87bd54bab6aa7766f854fd1dd96157767cf77e85
hamzasand <mhamzasaeed762@gmail.com>
2026-08-10 20:41:58 +0500

    start complete project with kubernetes

---
+++ 2.llmops-project2-chatgpt-kubernets/app.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from main import ask_chatbot
+import uvicorn
+
+app = FastAPI(
+    title ="Simple ChatBot API",
+    vesrion = "1.0"
+)
+
+class ChatRequest(BaseModel):
+    question: str
+
+class ChatResponse(BaseModel):
+    answer: str
+
+@app.get("/")
+def home():
+    return {
+        "message": "Chatbot API is running"
+    }
+
+@app.post("/chat",response_model=ChatResponse)
+def chat(request: ChatRequest):
+    answer = ask_chatbot(request.question)
+    return ChatResponse(answer=answer)
+
+# Entry point
+if __name__ == "__main__":
+    uvicorn.run(
+        "app:app",
+        host="127.0.0.1",
+        port=8000,
+        reload=True
+    )
---
+++ 2.llmops-project2-chatgpt-kubernets/main.py
@@ -0,0 +1,64 @@
+import os
+from langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+context = context = """
+My name is Muhammad Hamza.
+
+I am an AI Engineer with around 2.5 years of professional experience.
+
+Currently, I work at Prismatics Technologies.
+
+My expertise includes:
+- Python
+- Machine Learning
+- Deep Learning
+- Large Language Models (LLMs)
+- Generative AI
+- LangChain
+- LangGraph
+- Retrieval-Augmented Generation (RAG)
+- Multi-Agent AI Systems
+- FastAPI
+- Docker
+
+I have experience building AI-powered chatbots, document question-answering systems, and automation solutions using LangChain and modern LLMs.
+
+I enjoy learning new AI technologies and solving real-world business problems using artificial intelligence.
+
+My goal is to become an expert AI Engineer specializing in Generative AI and Agentic AI systems.
+"""
+
+llm = ChatMistralAI(
+    model="mistral-large-latest",
+    temperature=0 )
+
+prompt = ChatPromptTemplate.from_template(
+    """ You are a helpful AI assistant.
+
+Answer ONLY using the context below.
+
+If the answer is not found in the context, respond:
+"I don't know based on the provided context."
+
+Context:
+{context}
+
+Question:
+{question}
+"""
+)
+
+parser = StrOutputParser()
+
+chain = prompt | llm | parser
+
+
+def ask_chatbot(question: str):
+    return chain.invoke(
+        {
+            "context": context,
+            "question": question,
+        }
+    )
